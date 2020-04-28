efa12009a244509d3f9932314ce8dfc6165e47da
Gabriel Cangussu <gabrielcangussu@gmail.com>
2020-04-28 10:46:13 -0300

    enhance typescript config

--- packages/base/rules/typescript.js
+++ packages/base/rules/typescript.js
@@ -1,17 +1,6 @@
 module.exports = {
   "@typescript-eslint/adjacent-overload-signatures": "error",
-  "@typescript-eslint/array-type": "off",
-  "@typescript-eslint/ban-ts-comment": "off",
   "@typescript-eslint/ban-types": "error",
-  "@typescript-eslint/class-literal-property-style": "off",
-  "@typescript-eslint/consistent-type-assertions": "off",
-  "@typescript-eslint/consistent-type-definitions": "off",
-  "@typescript-eslint/explicit-function-return-type": "off",
-  "@typescript-eslint/explicit-member-accessibility": "off",
-  "@typescript-eslint/explicit-module-boundary-types": "off",
-  "@typescript-eslint/member-delimiter-style": "off",
-  "@typescript-eslint/member-ordering": "off",
-  "@typescript-eslint/method-signature-style": "off",
   "@typescript-eslint/naming-convention": [
     "error",
     {
@@ -19,7 +8,6 @@ module.exports = {
       format: ["PascalCase"],
     },
   ],
-  "@typescript-eslint/no-dynamic-delete": "off",
   "@typescript-eslint/no-empty-interface": "error",
   "@typescript-eslint/no-explicit-any": [
     "error",
@@ -31,22 +19,8 @@ module.exports = {
   "@typescript-eslint/no-misused-new": "error",
   "@typescript-eslint/no-namespace": "error",
   "@typescript-eslint/no-non-null-asserted-optional-chain": "error",
-  "@typescript-eslint/no-non-null-assertion": "off",
-  "@typescript-eslint/no-parameter-properties": "off",
-  "@typescript-eslint/no-require-imports": "off",
-  "@typescript-eslint/no-this-alias": "off",
-  "@typescript-eslint/no-type-alias": "off",
-  "@typescript-eslint/no-var-requires": "off",
   "@typescript-eslint/prefer-as-const": "warn",
-  "@typescript-eslint/prefer-for-of": "off",
-  "@typescript-eslint/prefer-function-type": "off",
-  "@typescript-eslint/prefer-namespace-keyword": "off",
-  "@typescript-eslint/prefer-optional-chain": "off",
-  "@typescript-eslint/prefer-ts-expect-error": "off",
   "@typescript-eslint/triple-slash-reference": "error",
-  "@typescript-eslint/type-annotation-spacing": "off",
-  "@typescript-eslint/typedef": "off",
-  "@typescript-eslint/unified-signatures": "off",
 
   // extension rules
   "brace-style": "off",
@@ -116,7 +90,19 @@ module.exports = {
   "space-before-function-paren": "off",
   "@typescript-eslint/space-before-function-paren": "off",
 
-  // eslint-plugin-import
+  // unneeded/conflicting eslint core rules
+  "getter-return": "off",
+  "no-dupe-args": "off",
+  "no-dupe-keys": "off",
+  "no-unreachable": "off",
+  "valid-typeof": "off",
+  "no-const-assign": "off",
+  "no-new-symbol": "off",
+  "no-this-before-super": "off",
+  "no-undef": "off",
+  "no-redeclare": "off",
+
+  // unneeded/conflicting eslint-plugin-import
   "import/named": "off",
   "import/default": "off",
   "import/namespace": "off",
